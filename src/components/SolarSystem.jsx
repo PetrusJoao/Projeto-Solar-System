2c971d45ebfeaa073330f1aa3f10dbf0ed2621df
João Pedro <joaopetrussousa@gmail.com>
2022-06-21 15:02:41 -0300

    Requisito 2

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <div data-testid="solar-system">Renderizei</div>
+    );
+  }
+}
+
+export default SolarSystem;
