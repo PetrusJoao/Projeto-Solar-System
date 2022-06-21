10a01cc1b3ee68358feae84dc8c03474a8e7f1c2
João Pedro <joaopetrussousa@gmail.com>
2022-06-21 16:14:21 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
+
 
 class App extends React.Component {
   render() {
@@ -8,6 +10,7 @@ class App extends React.Component {
       <div>
         <Header />
         <SolarSystem />
+        <Title headline="teste" />
       </div>
     );
   }
