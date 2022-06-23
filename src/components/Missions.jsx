c1bf009e688c6244f46984aa1c3afe42e576666e
João Pedro <joaopetrussousa@gmail.com>
2022-06-23 09:15:15 -0300

    Requisito 8

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 class Mission extends Component {
   render() {
     return (
-      <div data-testid="missions"> </div>
+      <div data-testid="missions">
+        <Title headline="Missões" />
+      </div>
     );
   }
 }
