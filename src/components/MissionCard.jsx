d548db917682bd5054ea1fd80939532f70398903
João Pedro <joaopetrussousa@gmail.com>
2022-06-25 09:42:57 -0300

    Requisito 10

--- src/components/MissionCard.jsx
+++ src/components/MissionCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MissionCard extends React.Component {
   render() {
@@ -21,5 +22,12 @@ class MissionCard extends React.Component {
     );
   }
 }
+
+MissionCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  year: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired,
+  destination: PropTypes.string.isRequired,
+};
 
 export default MissionCard;
--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Mission extends Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        { missions.map((mission) => (<MissionCard
+          name={ mission.name }
+          year={ mission.year }
+          country={ mission.country }
+          destination={ mission.destination }
+          key={ mission.name }
+        />))}
       </div>
     );
   }
